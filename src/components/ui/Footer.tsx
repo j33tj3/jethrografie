99d21b11d687dbd163fe90bc24084aa210ec4491
J33tj3 <j33tj3@gmail.com>
2024-02-16 10:51:57 +0100

    Set up Terms page

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="flex w-screen justify-center bg-black py-10 text-white">
+    <footer className="flex w-screen justify-center bg-black px-4 py-10 text-white">
       <div className="w-full max-w-5xl">
-        <Link href="">Algemene voorwaarden</Link>
+        <Link href="/algemenevoorwaarden">Algemene voorwaarden</Link>
       </div>
     </footer>
   );
